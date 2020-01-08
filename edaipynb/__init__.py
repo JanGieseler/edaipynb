5988773909c69cd19d27449bd8a0f6df437a92c4
Jan Gieseler <Jan.Gieseler@hotmail.com>
2020-01-08 14:48:02 +0100

    just testing setup.py install

--- edaipynb/__init__.py
+++ edaipynb/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1a2'
+__version__ = '0.1a3'
 
 # make the files available in the namespace of edaipynb
 from edaipynb.tools.notebook import save_notebook_as_html, save_fig, restart_kernel, get_notebook_name
